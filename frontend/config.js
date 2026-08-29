98ba47331c2080f140761502a52b1d365f7bcf87
Nehatere <137277833+Nehatere@users.noreply.github.com>
2026-08-29 04:11:46 -0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // After you deploy the backend with `sam deploy`, SAM prints an ApiUrl output.
 // Paste it here. It should look like:
 // https://abc123xyz.execute-api.ap-south-1.amazonaws.com/prod/tasks
-const API_URL = "https://tcq8w0zca0.execute-api.ap-south-1.amazonaws.com/prod/tasks";
+const API_URL = "https://v090s7wzvd.execute-api.ap-south-1.amazonaws.com/prod/tasks";
